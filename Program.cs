8da76f0788d9ed77185127badb57d724485a9df9
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-21 22:06:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TaskJunior2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberRandom;
+            int firstInput = 1;
+            int finishInput = 101;
+            int degreeValue = 1;
+            int baseNumber = 2;
+
+            Console.WriteLine("Приветствую вас в математическом МИРЕ.\n\n" +
+                              "В котором мы будем находить минимальную степень двойки, превосходящую заданное число.\n");
+
+            Console.WriteLine("Нажмите любую кнопку, чтобы создать случайное число.");
+            Console.ReadKey();
+
+            numberRandom = random.Next(firstInput, finishInput);
+
+            Console.WriteLine($"\n{numberRandom}");
+
+            while (baseNumber < numberRandom)
+            {
+                baseNumber *= degreeValue;
+                degreeValue++;
+            }
+
+            Console.WriteLine($"Минимальная степень {baseNumber}, превышающая заданное число, равна {degreeValue}.");
+        }
+    }
+}
